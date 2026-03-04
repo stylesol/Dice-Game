190daed958448471ec94792c98dbab5316180ce3
stylesol <stylesol@wgc.school.nz>
2026-03-04 13:09:49 +1300

    Add files via upload
    Created the dice support class.

---
+++ dice.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/**
+ * Write a description of class dice here.
+ *
+ * @author olivia
+ * @version 3/3/26
+ */
+public class dice
+{
+    private final int MAX = 6; //number of sides of dice
+    private int roll; 
+    
+    
+    // this is what runs the constructor
+    public dice(){
+        this.roll();
+    }
+    
+    //this is where it rolls
+    public void roll() {
+        roll = (int)(Math.random() * MAX);
+    }
+    
+    // this returns the number
+    
+    public int getRoll(){
+        return this.roll;
+    }
+}
