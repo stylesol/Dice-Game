3c52a3de87f30dc1bcdfa6a9eb14ce916ed38cfa
stylesol <stylesol@wgc.school.nz>
2026-03-04 13:32:46 +1300

    my driver class but it isnt working at this point

---
+++ driver.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+/**
+ * Write a description of class driver here.
+ *
+ * @author Olivia
+ * @version 4/03/26
+ */
+public class driver
+{
+    private int player1Roll; // person
+    private int player2Roll; // computer
+    private int dice1;
+    private int dice2;
+    
+    
+    private int round = 0; // create rounds for game
+    
+    
+    
+    public static void main(String[] args){
+        // create dice to flip
+        dice myDice = new dice();
+        
+        //for loop for dice class
+        
+}
+}
